849f2a1417a6ec5efa8ea343f297406910b27141
nagasawa <nagasawa944@gmail.com>
2023-01-23 11:25:21 +0900

    update readme

--- vite-tri/src/image/image.component.js
+++ vite-tri/src/image/image.component.js
@@ -2,5 +2,5 @@ import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-run
 import './image.component.css';
 import reactLogo from '../assets/react.svg';
 export function Test() {
-    return (_jsx(_Fragment, { children: _jsxs("div", { children: [_jsx("span", { children: "css/\u753B\u50CF \u8AAD\u307F\u8FBC\u307F\u30C1\u30A7\u30C3\u30AF\u7528\u30B3\u30F3\u30DD\u30FC\u30CD\u30F3\u30C8" }), _jsx("br", {}), _jsx("span", { children: _jsx("img", { className: "react-logo", src: reactLogo }) })] }) }));
+    return (_jsx(_Fragment, { children: _jsxs("div", { children: [_jsx("span", { children: "css/\u753B\u50CF \u8AAD\u307F\u8FBC\u307F\u30C1\u30A7\u30C3\u30AF\u7528\u30B3\u30F3\u30DD\u30FC\u30CD\u30F3\u30C8" }), _jsx("br", {}), _jsx("span", { children: _jsx("img", { className: "react-logo", src: reactLogo.name }) })] }) }));
 }
